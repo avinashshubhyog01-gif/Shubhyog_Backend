c89b8764c1944402519e7d07e4d3c918617dce2c
Kumari Anju Hembram <116105555+developeranjuUI@users.noreply.github.com>
2025-12-16 01:31:13 +0530

    Change OTP expiration time to 30 seconds

--- src/models/Otp.js
+++ src/models/Otp.js
@@ -4,7 +4,7 @@ const otpSchema = new mongoose.Schema({
   mobile: { type: String, required: true },
   otp: { type: String, required: true },
   role: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 300 } // expires after 5 minutes
+  createdAt: { type: Date, default: Date.now, expires: 30 } // expires after 30 seconds
 });
 
 const Otp = mongoose.model('Otp', otpSchema);
